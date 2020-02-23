b307d05c76c786ac56685d5437f6395113d5f819
Gekif <dzulfikar.maulana@gmail.com>
2020-02-23 13:20:52 +0700

    5. Programming The Operations for Nested Resources in the RESTful API: 2. Implementing the Methods to Insert Nested Resources

--- app/Http/Controllers/CourseStudentController.php
+++ app/Http/Controllers/CourseStudentController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Course;
+use App\Student;
 
 class CourseStudentController extends Controller
 {
     public function index($course_id)
     {
-        $course = Course::findOrFail($course_id);
+        $course = Course::find($course_id);
 
         if ($course) {
             $students = $course->students;
@@ -21,9 +22,32 @@ class CourseStudentController extends Controller
     }
 
 
-    public function store()
+    public function store($course_id, $student_id)
     {
-        return __METHOD__;
+        $course = Course::find($course_id);
+
+        if ($course) {
+            $student = Student::find($student_id);
+
+            if ($student) {
+
+                if ($course->students()->find($student->id)) {
+                    return $this->createErrorResponse(
+                        "The student with id {$student_id} already exists in the course with id {$course_id}", 409);
+                }
+
+                $course->students()->attach($student->id);
+
+                return $this->createSuccessResponse(
+                    "The student with id {$student_id} was added to the course with {$course->id}", 201);
+            }
+
+            return $this->createErrorResponse(
+                "The student with id {$student_id}, doesn't exists", 404);
+        }
+
+        return $this->createErrorResponse(
+            "The course with id {$course_id}, doesn't exist", 404);
     }
 
 
--- app/Http/Controllers/TeacherCourseController.php
+++ app/Http/Controllers/TeacherCourseController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Course;
 use App\Teacher;
+use Illuminate\Http\Request;
 
 class TeacherCourseController extends Controller
 {
     public function index($teacher_id)
     {
-        $teacher = Teacher::findOrFail($teacher_id);
+        $teacher = Teacher::find($teacher_id);
 
         if ($teacher) {
             $courses = $teacher->courses;
@@ -17,13 +19,30 @@ class TeacherCourseController extends Controller
         }
 
         return $this->createErrorResponse(
-            'Does not exists a teacher with the given id', 404);
+            "The teacher with id {$teacher_id} doesn't exists",
+            404);
     }
 
 
-    public function store()
+    public function store(Request $request, $teacher_id)
     {
-        return __METHOD__;
+        $teacher = Teacher::find($teacher_id);
+
+        if ($teacher) {
+            $this->validateRequest($request);
+
+            $course = Course::create([
+                'title' => $request->get('title'),
+                'description' => $request->get('description'),
+                'value' => $request->get('value'),
+                'teacher_id' => $teacher->id,
+            ]);
+
+            return $this->createSuccessResponse("The course with id {$course->id} has been created and associated with the teacher with id {$teacher->id}"
+                , 201);
+        }
+
+        return $this->createErrorResponse("The teacher with id {$teacher_id} does not exists", 404);
     }
 
 
@@ -43,4 +62,16 @@ class TeacherCourseController extends Controller
     {
         return __METHOD__;
     }
+
+    function validateRequest($request)
+    {
+        $rules = [
+            'title' => 'required',
+            'description' => 'required',
+            'value' => 'required|numeric'
+        ];
+
+        $this->validate($request, $rules);
+    }
+
 }
